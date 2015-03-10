a26a1bc017099b6f4d81e5f2c86558777464aee2
Anna <anne-marie.therien-daniel@umontreal.ca>
2015-03-10 16:01:32 -0400

    Anna test

--- app/src/main/java/com/example/gab/helloworld/MainActivity.java
+++ app/src/main/java/com/example/gab/helloworld/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MonPremierToast", "OnCreate appele!");
+        Log.d("MonPremierToast", "OnCreate annabanana!");
     }
 
 
